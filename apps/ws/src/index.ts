141ed4f820638f09b18de73b3e564015f1b25163
sushantaruidas <sushantaruidas95@gmail.com>
2025-12-27 00:33:49 +0530

    feat(ws): add JWT verification for WebSocket connections and update dependencies

--- apps/ws/src/index.ts
+++ apps/ws/src/index.ts
@@ -1,7 +1,29 @@
 import {WebSocketServer} from "ws"
+import jwt from "jsonwebtoken"
+import {JWT_SECRET} from "@repo/backend-common"
 const wss= new WebSocketServer({port:8080})
-wss.on("connection", (ws)=>{
-    ws.on("message",()=>{
-        
-    })
+wss.on("connection", (ws, request)=>{
+    const url=request.url
+    if(!url){
+        ws.send(JSON.stringify({
+            type:"error",
+            msg:"Invalid url"
+        }))
+        ws.close(1008,"Invalid url")
+        return
+    }
+    const queryParams=new URLSearchParams(url.split('?')[1])
+    const token=queryParams.get('token')
+    if(!token){
+        return
+    }
+   
+    const decoded= jwt.verify(token,JWT_SECRET)
+     if(typeof decoded== "string"){
+        return
+    }
+    if(!decoded.userId){
+        ws.close()
+        return
+    }
 })
