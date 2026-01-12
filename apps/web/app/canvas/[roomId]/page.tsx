93a5b8a541db64181455995d54ac4bc065754b57
sushantaruidas <sushantaruidas95@gmail.com>
2026-01-12 22:03:44 +0530

    Refactor code structure for improved readability and maintainability

--- apps/web/app/canvas/[roomId]/page.tsx
+++ apps/web/app/canvas/[roomId]/page.tsx
@@ -7,14 +7,37 @@ function page() {
       if(canvasRef.current){
         const canvas= canvasRef.current
         const ctx= canvas.getContext("2d")
-        ctx?.fillRect(25,25,100,100)
+        if(!ctx){
+          return
+        }
+        let clicked= false
+        let startX=0
+        let startY=0
+        canvas.addEventListener("mousedown", (e)=>{
+          clicked=true
+          startX=e.clientX
+          startY=e.clientY
+        })
+        canvas.addEventListener("mouseup", (e)=>{
+          clicked=false
+          startX=e.clientX
+          startY=e.clientY
+        })
+        canvas.addEventListener("mousemove", (e)=>{
+          if(clicked){
+            const width=e.clientX - startX
+            const height= e.clientY - startY
+            ctx.clearRect(0, 0, canvas.width, canvas.height)
+            ctx.strokeRect(startX, startY, width, height)
+          }
+        })
       }
 
     },[canvasRef]
   )
   return (
     <div>
-      <canvas ref={canvasRef} width={500} height={500}></canvas>
+      <canvas ref={canvasRef} width={1080} height={1200}></canvas>
     </div>
   )
 }
--- apps/ws/src/index.ts
+++ apps/ws/src/index.ts
@@ -1,36 +1,52 @@
-import {WebSocketServer} from "ws"
-import jwt from "jsonwebtoken"
-import {JWT_SECRET} from "@repo/backend-common"
+import { WebSocket, WebSocketServer } from "ws";
 const wss= new WebSocketServer({port:8080})
-function checkUser(token:string):string|null{
-    const decoded= jwt.verify(token,JWT_SECRET)
-     if(typeof decoded== "string"){
-        return null
-    }
-    if(!decoded.userId){
-
-        return null
+import jwt from "jsonwebtoken"
+import {JWT_SECRET} from '@repo/backend-common'
+interface User{
+  userId: string,
+  rooms: string[],
+  ws: WebSocket
+}
+const users: User[]=[]
+function verifyUser(token:string): string| null{
+  try {
+    const decoded= jwt.verify(token, JWT_SECRET)
+    if(typeof decoded=== "string"){
+      return null
     }
     return decoded.userId
+  } catch (error) {
+    return null
+  }
 }
-wss.on("connection", (ws, request)=>{
-    const url=request.url
-    if(!url){
-        ws.send(JSON.stringify({
-            type:"error",
-            msg:"Invalid url"
-        }))
-        ws.close(1008,"Invalid url")
+wss.on('connection',( ws, request)=>{
+  const url=request.url
+  if(!url){
+    return
+  }
+  const queryParamas= new URLSearchParams(url.split('?')[1])
+  const token= queryParamas.get("token")
+  console.log(token);
+  if(!token){
+    return
+  }
+  const userId= verifyUser(token)
+  if(!userId){
+    ws.close()
+    return
+  }
+  ws.on('message', function message(data){
+    const parsedData= JSON.parse(data.toString())
+    if(parsedData=== "join_room"){
+      const user= users.find(x=>x.ws=== parsedData.roomId)
+      user?.rooms.push(parsedData.roomId)
+    }
+    if(parsedData=== "leave_room"){
+      const user= users.find(x=>x.ws===parsedData.roomId)
+      if(!user){
         return
+      }
+      user.rooms= user?.rooms.filter(x=>x=== parsedData.roomId)
     }
-    const queryParams=new URLSearchParams(url.split('?')[1])
-    const token=queryParams.get('token')
-    if(!token){
-        return
-    }
-    const userId= checkUser(token)
-    if(!userId){
-        ws.close()
-    }
-    
+  })
 })
