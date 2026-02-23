1fcda1007271c4dc14c731f5daa71488a9fba079
sushantaruidas <sushantaruidas95@gmail.com>
2026-02-23 20:39:42 +0530

    feat: initialize database schema with User, Room, and Chat tables and their relationships

--- packages/database/prisma/migrations/20260223144138_init/migration.sql
+++ packages/database/prisma/migrations/20260223144138_init/migration.sql
@@ -1,4 +1,17 @@
 -- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "photo" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "Room" (
     "id" SERIAL NOT NULL,
     "slug" TEXT NOT NULL,
@@ -17,6 +30,12 @@ CREATE TABLE "Chat" (
 
     CONSTRAINT "Chat_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Room_slug_key" ON "Room"("slug");
